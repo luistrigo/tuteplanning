55e69af987c997d1a120b841636ce766e0c34d67
luistrigoc <luistrigoc@hotmail.com>
2022-11-05 22:45:44 +0100

    Update versions and change test config to vitest :wrench:

--- cypress.config.js
+++ cypress.config.js
@@ -4,12 +4,5 @@ module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:4173'
-  },
-  component: {
-    specPattern: 'src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
-    devServer: {
-      framework: 'vue',
-      bundler: 'vite'
-    }
   }
 })
